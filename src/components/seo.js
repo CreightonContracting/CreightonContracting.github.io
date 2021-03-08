d35f667a06d4f818cc4c5b86fa98f40d9b9fab99
Michael Butler <michael_w_butler@live.com>
2021-03-08 15:25:15 -0700

    Adding other metatags for seo

--- src/components/seo.js
+++ src/components/seo.js
@@ -11,6 +11,17 @@ const SEO = () => {
           including all underground, grading, and paving.  Creighton Contracting utilizes a modern fleet of 
           maintained equipment and GPS systems to reduce time and cost while improving accuracy and quality on projects."
       />
+      <meta name="url" content="https://www.creightoncontracting.com/" />
+      <meta name="image" content="../images/Creighton_Contracting_Logo_White.png" />
+      <meta name="author" content="Creighton Contracting LLC" />
+      <meta property="og:url" content="https://www.creightoncontracting.com/"/>
+      {/* <meta property="og:type" content=""/> */}
+      <meta property="og:title" content="Creighton Contracting LLC"/>
+      <meta property="og:description" content="Creighton Contracting LLC was established in 2017, and provides turn key packages for site work, 
+          including all underground, grading, and paving.  Creighton Contracting utilizes a modern fleet of 
+          maintained equipment and GPS systems to reduce time and cost while improving accuracy and quality on projects."
+      />
+      <meta property="og:image" content="../images/Creighton_Contracting_Logo_White.png" />
     </Helmet>
   )
 }
@@ -19,4 +30,7 @@ export default SEO
 
 // Add english lang tab
 // add features to make this searchable 
-//\
+// url
+// image
+// twitter
+// author
